7d240744c6165164b0ea004fc212b649a3b433f8
Somnath <somnath.kadam@consisty.com>
2024-10-30 15:27:06 +0530

    feat(test): base code

--- android/src/main/java/com/consistypayswiffmatm/ConsistyPayswiffMatmPackage.kt
+++ android/src/main/java/com/consistypayswiffmatm/ConsistyPayswiffMatmPackage.kt
@@ -1,6 +1,7 @@
 package com.consistypayswiffmatm
 
 import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.JavaScriptModule
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
@@ -9,6 +10,10 @@ import com.facebook.react.uimanager.ViewManager
 class ConsistyPayswiffMatmPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
     return listOf(ConsistyPayswiffMatmModule(reactContext))
+  }
+
+  override fun createJSModules(): MutableList<Class<out JavaScriptModule>> {
+    TODO("Not yet implemented")
   }
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
